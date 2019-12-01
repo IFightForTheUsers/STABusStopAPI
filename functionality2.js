affd3868150b9b722bd8224b63c1a97ffa97069a
Steven Richmond <srichmond@eagles.ewu.edu>
2019-12-01 14:49:11 -0800

    added user ability to change color scheme

--- functionality2.js
+++ functionality2.js
@@ -167,3 +167,10 @@ function timeConverter(UNIX_timestamp){
 
     
 }
+
+$("#setNewColorScheme").click(setNewColors);
+
+function setNewColors() {
+    document.getElementById("stopInfo").style.background = document.getElementById("color1").value;
+    document.getElementById("tripInfo").style.background = document.getElementById("color2").value;
+}
